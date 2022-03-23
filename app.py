ffb6cf895eb08cc1a28757815f8f5b3cfbdbe807
Your Name <you@example.com>
2022-03-23 22:29:50 +0100

    first commit

--- app.py
+++ app.py
@@ -1,45 +1,57 @@
 from flask import Flask 
 from flask_socketio import SocketIO, send,join_room, leave_room,emit
-# from models import db,User,Room,Message,Participants
+from models import db,User,Room,Message,Participants
 from flask_cors import CORS
 
-# from flask_admin import Admin
-# from flask_admin.contrib.sqla import ModelView
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
-# db.init_app(app)
+db.init_app(app)
 app.config['SECRET_KEY'] = 'mysecret'
 CORS(app)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] ="postgresql://sqfchhvcywmegt:1e4f30c9447087c90170fe999bdbd7cdf542f57f61dbe368baaeb2e358a62329@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/d3e165le2np51u"
-# "sqlite:///test.db"
+app.config['SQLALCHEMY_DATABASE_URI'] ="postgres://dmajucpgzfjpef:ed7e7cd83bb95527cb30d83b3bfcebcb749e5fc2effea616b1860311a4adbf6a@ec2-54-73-178-126.eu-west-1.compute.amazonaws.com:5432/dat8693bl607v1"
+# app.config['SQLALCHEMY_DATABASE_URI'] ="sqlite:///test.db"
 socketio = SocketIO(app, cors_allowed_origins='*')
-# admin = Admin(app)
-# admin.add_view(ModelView(User, db.session))
-# admin.add_view(ModelView(Room, db.session))
-# admin.add_view(ModelView(Message, db.session))
-# admin.add_view(ModelView(Participants, db.session))
-
+admin = Admin(app)
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Room, db.session))
+admin.add_view(ModelView(Message, db.session))
+admin.add_view(ModelView(Participants, db.session))
 
 @socketio.on('message')
-def on_joinoooo(msg):
-	send(msg,broadcast=True)
+def handleMessage(data):
+	new_msg = Message(
+			user_id=data['user_id'], 
+			room_id=data['room_id'], 
+			msg=data['msg']
+		    )
+	Us = [i.user_id for i in Participants.query.filter_by(room_id=data['user_id']).all()]
+	print(Us)
+	db.session.add(new_msg)
+	db.session.commit()
+	send({"user1":Us[0],"user2":Us[1]},broadcast=True)
 
-@socketio.on('join')#, namespace='/private')
-def on_join(data):
-	user = data['username']
-	print('join room1')
-	room=data['room']
-	join_room(room)
-	send(f'{user} has entered the {room}',room=room)
+# @socketio.on('message')
+# def on_joinoooo(msg):
+# 	send(msg,broadcast=True)
 
-@socketio.on('private_message')#, namespace='/private')
-def private_message(payload):
-	user = payload['username']
-	message = payload['message']
-	room=payload['room']
+# @socketio.on('join')#, namespace='/private')
+# def on_join(data):
+# 	user = data['username']
+# 	print('join room1')
+# 	room=data['room']
+# 	join_room(room)
+# 	send(f'{user} has entered the {room}',room=room)
 
-	send(f"{room} |{user} :{message} ." ,room=room)
+# @socketio.on('private_message')#, namespace='/private')
+# def private_message(payload):
+# 	user = payload['username']
+# 	message = payload['message']
+# 	room=payload['room']
+
+# 	send(f"{room} |{user} :{message} ." ,room=room)
 
 
 # @socketio.on('join')
